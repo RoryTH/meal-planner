cbefd366b2b83eac522a69bb181901cc09d94195
Rory Holmes <rorythomas511@gmail.com>
2023-12-28 15:06:21 +0000

    Add "Epicurist" branding to navbar and html head

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -29,8 +29,9 @@ const NavBar = () => {
     return (
         <nav className="text-lg bg-stone-200 dark:bg-gray-900 text-gray-800 dark:text-stone-100 fixed w-full z-20 top-0 start-0 border-b border-stone-100 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link href="/">
+                <Link href="/" className="flex items-center gap-2 text-2xl">
                     <PiBowlFoodFill size={50} />
+                    Epicurist
                 </Link>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                     <ThemeToggle />
